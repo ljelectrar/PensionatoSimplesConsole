499ee8ba769f11316edd77ef4d453eb36d491095
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-11-19 20:46:10 -0300

    Atualizaçao para commit 2

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,55 @@
+package application;
+
+import entities.Hospede;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+       Hospede[] hospedes = new Hospede[10];
+
+        System.out.print("Quantos quartos vão ser alugados? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i <= n; i++){
+            sc.nextLine();
+            System.out.println("RENT #" + i + ":");
+
+            System.out.println("NOME: ");
+            String nome = sc.nextLine();
+
+            System.out.println("Email: ");
+            String email = sc.nextLine();
+
+            System.out.println("QUARTO: ");
+            int quarto = sc.nextInt();
+
+            hospedes[quarto] = new Hospede(nome, email);
+        }
+
+        System.out.println();
+        System.out.println("Quartos Ocupados: ");
+        for (int i = 0; i <= 9; i++){
+            if (hospedes[i] != null){
+                System.out.println(i + ": " + hospedes[i]);
+            }
+        }
+
+        System.out.println();
+        System.out.println("Quartos Livres: ");
+        for (int i = 0; i <= 9; i++) {
+            if(hospedes[i] == null) {
+                System.out.println(": " + i);
+            }
+        }
+
+        sc.close();
+
+        System.out.println("Fim e teste Commit");
     }
 }
